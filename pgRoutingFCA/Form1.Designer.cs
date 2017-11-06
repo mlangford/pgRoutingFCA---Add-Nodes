17a26dd603bc6c5c8fe261e2d973b15caf5cf874
Mitchel Langford <mlangfor@glam.ac.uk>
2017-11-06 09:43:35 +0000

    Form1 installed - gather connection details

--- pgRoutingFCA/Form1.Designer.cs
+++ pgRoutingFCA/Form1.Designer.cs
@@ -29,11 +29,208 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.lbFeedback = new System.Windows.Forms.Label();
+            this.btTest = new System.Windows.Forms.Button();
+            this.btGo = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lbFeedback
+            // 
+            this.lbFeedback.AutoSize = true;
+            this.lbFeedback.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbFeedback.ForeColor = System.Drawing.Color.MistyRose;
+            this.lbFeedback.Location = new System.Drawing.Point(34, 281);
+            this.lbFeedback.Name = "lbFeedback";
+            this.lbFeedback.Size = new System.Drawing.Size(114, 25);
+            this.lbFeedback.TabIndex = 62;
+            this.lbFeedback.Text = "lbFeedback";
+            this.lbFeedback.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.lbFeedback.Visible = false;
+            // 
+            // btTest
+            // 
+            this.btTest.BackColor = System.Drawing.Color.DimGray;
+            this.btTest.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btTest.ForeColor = System.Drawing.SystemColors.Window;
+            this.btTest.Location = new System.Drawing.Point(32, 242);
+            this.btTest.Name = "btTest";
+            this.btTest.Size = new System.Drawing.Size(441, 33);
+            this.btTest.TabIndex = 61;
+            this.btTest.Text = "test connection";
+            this.btTest.UseVisualStyleBackColor = false;
+            this.btTest.Click += new System.EventHandler(this.btTest_Click);
+            // 
+            // btGo
+            // 
+            this.btGo.BackColor = System.Drawing.Color.DimGray;
+            this.btGo.Enabled = false;
+            this.btGo.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btGo.ForeColor = System.Drawing.SystemColors.Window;
+            this.btGo.Location = new System.Drawing.Point(32, 323);
+            this.btGo.Name = "btGo";
+            this.btGo.Size = new System.Drawing.Size(441, 33);
+            this.btGo.TabIndex = 51;
+            this.btGo.Text = "OK";
+            this.btGo.UseVisualStyleBackColor = false;
+            this.btGo.Click += new System.EventHandler(this.btGo_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Window;
+            this.label4.Location = new System.Drawing.Point(27, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 28);
+            this.label4.TabIndex = 60;
+            this.label4.Text = "Database:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.DimGray;
+            this.textBox5.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox5.Location = new System.Drawing.Point(200, 186);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(273, 35);
+            this.textBox5.TabIndex = 56;
+            this.textBox5.Text = "usw_allwales";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.DimGray;
+            this.textBox4.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox4.Location = new System.Drawing.Point(385, 132);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(88, 35);
+            this.textBox4.TabIndex = 55;
+            this.textBox4.Text = "5432";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Window;
+            this.label3.Location = new System.Drawing.Point(27, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 28);
+            this.label3.TabIndex = 59;
+            this.label3.Text = "Server/Port:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.DimGray;
+            this.textBox3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Location = new System.Drawing.Point(200, 132);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(179, 35);
+            this.textBox3.TabIndex = 54;
+            this.textBox3.Text = "localhost";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Window;
+            this.label2.Location = new System.Drawing.Point(27, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 28);
+            this.label2.TabIndex = 58;
+            this.label2.Text = "Password:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.DimGray;
+            this.textBox2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox2.Location = new System.Drawing.Point(200, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '~';
+            this.textBox2.Size = new System.Drawing.Size(273, 35);
+            this.textBox2.TabIndex = 53;
+            this.textBox2.Text = "Password$1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(27, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 28);
+            this.label1.TabIndex = 57;
+            this.label1.Text = "Username:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.DimGray;
+            this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(200, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(273, 35);
+            this.textBox1.TabIndex = 52;
+            this.textBox1.Text = "postgres";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(500, 389);
+            this.Controls.Add(this.lbFeedback);
+            this.Controls.Add(this.btTest);
+            this.Controls.Add(this.btGo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "pgRoutingFCA ~ PostGIS connection";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbFeedback;
+        private System.Windows.Forms.Button btTest;
+        private System.Windows.Forms.Button btGo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- pgRoutingFCA/Form1.cs
+++ pgRoutingFCA/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Npgsql;
 
 namespace pgRoutingFCA
 {
@@ -15,6 +10,91 @@ namespace pgRoutingFCA
         public Form1()
         {
             InitializeComponent();
+        }
+
+        string conString;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("lkg.txt"))
+                {
+                    textBox1.Text = sr.ReadLine();
+                    textBox2.Text = sr.ReadLine();
+                    textBox3.Text = sr.ReadLine();
+                    textBox4.Text = sr.ReadLine();
+                    textBox5.Text = sr.ReadLine();
+                }
+            }
+            catch (Exception)
+            {
+                //just carry on with default settings
+            }
+        }
+
+        private void btTest_Click(object sender, EventArgs e)
+        {
+            conString = "Server=" + textBox3.Text +
+                                           "; Port=" + textBox4.Text +
+                                           "; User Id=" + textBox1.Text +
+                                           "; Password=" + textBox2.Text +
+                                           "; Database=" + textBox5.Text;
+            try
+            {
+                using (NpgsqlConnection dbConnection = new NpgsqlConnection(conString))
+                {
+                    dbConnection.Open();
+                    dbConnection.Close();
+                    btGo.Enabled = true;
+                    showlabel("Connection settings OK", 4000);
+                    btGo.Focus();
+                }
+            }
+            catch (Exception)
+            {
+                showlabel("Connection Failed ~ review settings", 4000);
+                btGo.Enabled = false;
+            }
+        }
+
+        private void btGo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //save Last Known Good details
+                using (StreamWriter sw = new StreamWriter("lkg.txt", false))
+                {
+                    sw.WriteLine(textBox1.Text);
+                    sw.WriteLine(textBox2.Text);
+                    sw.WriteLine(textBox3.Text);
+                    sw.WriteLine(textBox4.Text);
+                    sw.WriteLine(textBox5.Text);
+                }
+            }
+            catch (Exception)
+            {
+                //just carry on
+            }
+            //Form2 frm2 = new Form2(conString);
+            //frm2.Owner = this;
+            this.Hide();
+            //frm2.Show();
+        }
+
+        //display a feedback label
+        private void showlabel(string detail, int time)
+        {
+            lbFeedback.Text = detail;
+            lbFeedback.Visible = true;
+            timer1.Interval = time;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lbFeedback.Visible = false;
+            timer1.Stop();
         }
     }
 }
